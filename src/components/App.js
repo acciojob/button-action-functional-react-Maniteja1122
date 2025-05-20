7f3a2d89293ddb4ae5fe4fd0dd26e1bcbcfd0f90
Maniteja1122 <kuntamaniteja0607@gmail.com>
2025-05-20 13:42:21 +0530

    Button action function

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [text, setText] = useState(null);
+  const handleClick = () => {
+    setText(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        click me
+      </button>
+     {text ? <p>{text}</p>:<p>please click the button</p>}
     </div>
   );
 }
-
 
 export default App;
