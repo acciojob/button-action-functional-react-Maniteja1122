65a60823749c0298103710d591ab4dd495f418d5
Maniteja1122 <kuntamaniteja0607@gmail.com>
2025-06-05 11:33:36 +0530

    resolved the button function

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text, setText] = useState(null);
+  const [text, setText] = React.useState(false);
   const handleClick = () => {
-    setText(
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
-    );
+    setText(true);
   };
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         click me
       </button>
-     {text ? <p>{text}</p>:<p>please click the button</p>}
+     {text && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
     </div>
   );
 }
